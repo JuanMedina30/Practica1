f799c3dc4a1fbab23fc280b7f64cc34880068b3c
Juan José Medina Ramos <99295867+JuanMedina30@users.noreply.github.com>
2022-04-27 14:36:54 -0700

    Update Programa.s

--- Programa.s
+++ Programa.s
@@ -8,12 +8,12 @@
 .global _start
 ############################# Code segment ####################################
 _start:
-  MOV R7, #4 ###;- cargar el valor de datos que esta en esta posicion en la secuencia de instrucciones
-  MOV R0, #1 ###;- cargar el valor de datos que esta en esta posicion en la secuencia de instrucciones
-  MOV R2, #12 ###;- parametro
-  LDR R1, =string ###parametro y direccion de la cadena
+  MOV R7, 4 ###;- cargar el valor de datos que esta en esta posicion en la secuencia de instrucciones
+  MOV R0, 1 ###;- cargar el valor de datos que esta en esta posicion en la secuencia de instrucciones
+  MOV R2, 12 ###;- parametro
+  LDR R1,string ###parametro y direccion de la cadena
   SWI 0 ### señal de interrupcion al O.S.
-  MOV R7, #1 ### salir del programa y matar el proceso
+  MOV R7,1 ### salir del programa y matar el proceso
   SWI 0 ### señal de interrupcion al O.S. para la terminacion
 ############################# Data segment ###################################
   .data
