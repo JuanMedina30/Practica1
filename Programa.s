c6c94c3a34549ff396fa7a997a418585d425e860
Juan José Medina Ramos <99295867+JuanMedina30@users.noreply.github.com>
2022-04-29 19:45:34 -0700

    Update Programa.s

--- Programa.s
+++ Programa.s
@@ -1,38 +1,25 @@
-.LC0:
-        .ascii  "Hola mundo sexo\000"
 main:
-        push    {r7, lr}
-        add     r7, sp, #0
-        movw    r0, #:lower16:.LC0
-        movt    r0, #:upper16:.LC0
-        bl      printf
-        movs    r3, #0
-        mov     r0, r3
-        pop     {r7, pc}
-__static_initialization_and_destruction_0(int, int):
-        push    {r7, lr}
+        push    {r11, lr}
+        mov     r11, sp
         sub     sp, sp, #8
-        add     r7, sp, #0
-        str     r0, [r7, #4]
-        str     r1, [r7]
-        ldr     r3, [r7, #4]
-        cmp     r3, #1
-        bne     .L5
-        ldr     r3, [r7]
-        movw    r2, #65535
-        cmp     r3, r2
-        bne     .L5
-        movw    r0, #:lower16:_ZStL8__ioinit
-        movt    r0, #:upper16:_ZStL8__ioinit
-        bl      std::ios_base::Init::Init() [complete object constructor]
-        movw    r2, #:lower16:__dso_handle
-        movt    r2, #:upper16:__dso_handle
-        movw    r1, #:lower16:_ZNSt8ios_base4InitD1Ev
-        movt    r1, #:upper16:_ZNSt8ios_base4InitD1Ev
-        movw    r0, #:lower16:_ZStL8__ioinit
-        movt    r0, #:upper16:_ZStL8__ioinit
-        bl      __aeabi_atexit
-.L5:
+        ldr     r0, .LCPI1_0
+        bl      printf
+        mov     r1, #0
+        str     r0, [sp, #4]                    @ 4-byte Spill
+        mov     r0, r1
+        mov     sp, r11
+        pop     {r11, lr}
+        bx      lr
+.LCPI1_0:
+        .long   .L.str
+_GLOBAL__sub_I_example.cpp:
+        push    {r11, lr}
+        mov     r11, sp
+        bl      __cxx_global_var_init
+        pop     {r11, lr}
+        bx      lr
+.L.str:
+        .asciz  "Hola mundo sexos"
         nop
         adds    r7, r7, #8
         mov     sp, r7
